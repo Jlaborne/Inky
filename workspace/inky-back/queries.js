adaf6e2538f7d18715c0035d82d536c4a29248b0
Jlaborne <laborne.j@hotmail.fr>
2024-10-04 20:08:28 +0200

    Rework of user CRUD

--- workspace/inky-back/queries.js
+++ workspace/inky-back/queries.js
@@ -1,32 +1,53 @@
+const User = require('./model/user.js');
 const Pool = require("pg").Pool;
+
 const pool = new Pool({
-  user: "joshua",
+  user: "postgres",
   host: "localhost",
-  database: "api",
+  database: "inky-db",
   password: "nitenboy53",
   port: 5432,
 });
-const getUsers = (request, response) => {
-  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
+const getUsers = async () => {
+  try {
+    let resultQuery = await pool.query("SELECT * FROM users ORDER BY id ASC");
+    let result = [];
+    resultQuery.rows.forEach((element) => result.push(new User(element.id, element.name, element.email)));
+    return result;
+  }
+
+  catch (error) {
+    console.log(`getUsers erreur : ${error}`);
+    throw error;
+  }
 };
 
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
+const getUserById = async (id) => {
+  try {
+    let resultQuery = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    let result = resultQuery.rows[0];
+    return new User(result.id, result.name, result.email);
+  }
+  catch (error) {
+    console.log(`getUserById erreur : ${error}`);
+    throw error;
+  }
 };
 
-const createUser = (request, response) => {
+const createUser = async (user) => {
+  try {
+    let resultQuery = await pool.query("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+      [user.name, user.email]
+    );
+    user.id = resultQuery.rows[0].id;
+    return user;
+  }
+  catch (error) {
+    console.log(`createUser erreur : ${error}`);
+    throw error;
+  }
+};
+/*const createUser = (request, response) => {
   const { name, email } = request.body;
 
   pool.query(
@@ -39,9 +60,21 @@ const createUser = (request, response) => {
       response.status(201).send(`User added with ID: ${results.rows[0].id}`);
     }
   );
-};
+};*/
 
-const updateUser = (request, response) => {
+const updateUser = async (user) => {
+  try {
+    let resultQuery = await pool.query("UPDATE users SET name = $1, email = $2 WHERE id = $3",
+      [user.name, user.email, user.id]
+    );
+    return user;
+  }
+  catch (error) {
+    console.log(`updateUser erreur : ${error}`);
+    throw error;
+  }
+};
+/*const updateUser = (request, response) => {
   const id = parseInt(request.params.id);
   const { name, email } = request.body;
 
@@ -55,9 +88,19 @@ const updateUser = (request, response) => {
       response.status(200).send(`User modified with ID: ${id}`);
     }
   );
-};
+};*/
 
-const deleteUser = (request, response) => {
+
+const deleteUser = async(id) => {
+  try {
+    await pool.query("DELETE FROM users WHERE id = $1", [id]);
+  }
+  catch (error) {
+    console.log(`updateUser erreur : ${error}`);
+    throw error;
+  }
+};
+/*const deleteUser = (request, response) => {
   const id = parseInt(request.params.id);
 
   pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
@@ -66,7 +109,7 @@ const deleteUser = (request, response) => {
     }
     response.status(200).send(`User deleted with ID: ${id}`);
   });
-};
+};*/
 
 module.exports = {
   getUsers,
--- workspace/inky-back/server.js
+++ workspace/inky-back/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-//const bodyParser = require('body-parser')
-const app = express();
+const User = require('./model/user.js');
 const db = require("./queries");
+
+const app = express();
 const port = 5000;
 
 app.use(cors());
@@ -17,11 +18,62 @@ app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-app.get("/users", db.getUsers);
-app.get("/users/:id", db.getUserById);
-app.post("/users", db.createUser);
-app.put("/users/:id", db.updateUser);
-app.delete("/users/:id", db.deleteUser);
+// 
+app.get("/users", async (request, response) => {
+  try {
+    let result = await db.getUsers();
+    response.status(200).json(result);
+  }
+  catch (error) {
+    response.status(500).send(" Erreur !");
+  }
+});
+
+app.get("/users/:id", async (request, response) => {
+  try {
+    const id = parseInt(request.params.id);
+    let result = await db.getUserById(id);
+    response.status(200).json(result);
+  }
+  catch (error) {
+    response.status(404).send(" Erreur !");
+  }
+});
+
+//app.post("/users", db.createUser);
+app.post("/users", async (request, response) => {
+  try {
+    let user = new User(-1, request.body.name, request.body.email);
+    let result = await db.createUser(user);
+    response.status(201).json(result);
+  }
+  catch (error) {
+    response.status(500).send(" Erreur !");
+  }
+});
+
+//app.put("/users/:id", db.updateUser);
+app.put("/users/:id", async (request, response) => {
+  try {
+    let user = new User(request.params.id, request.body.name, request.body.email);
+    let result = await db.updateUser(user);
+    response.status(200).json(result);
+  }
+  catch (error) {
+    response.status(500).send(" Erreur !");
+  }
+});
+
+//app.delete("/users/:id", db.deleteUser);
+app.delete("/users/:id", async (request, response) => {
+  try {
+    await db.deleteUser(request.params.id);
+    response.status(200).send("Supression éffectuée");
+  }
+  catch (error) {
+    response.status(500).send(" Erreur !");
+  }
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
